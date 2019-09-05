636fb14f6f640bfb51b2486ca797bf63ecd1c1bb
Thomas Lundgren <thomasLundgren@live.com>
2019-09-05 21:29:11 +0200

    MOVED CHALLENGES TO OWN DIRECTORIES

--- ottergram/scripts/link-hijack.js
+++
@@ -1,8 +0,0 @@
-var links = [].slice.apply(document.getElementsByTagName('a'));
-
-links.forEach(function(link) {
-
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
-  });
-});
