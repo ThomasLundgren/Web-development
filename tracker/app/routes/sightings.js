8efdb6ef33f37ad078abcd27730843879b6ad560
Thomas Lundgren <thomasLundgren@live.com>
2019-10-14 12:43:37 +0200

    Chapter 20 ACTUALLY done

--- tracker/app/routes/sightings.js
+++ tracker/app/routes/sightings.js
@@ -1,4 +1,38 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default Route.extend({
+  model() {
+    return [
+      {
+        id: 1,
+        location: "Asilomar",
+        sightedAt: new Date("2019-10-14")
+      },
+      {
+        id: 2,
+        location: "Asilomar",
+        sightedAt: new Date("2019-10-14")
+      },
+      {
+        id: 3,
+        location: "Asilomar",
+        sightedAt: new Date("2019-10-14")
+      },
+      {
+        id: 4,
+        location: "Asilomar",
+        sightedAt: new Date("2019-10-14")
+      },
+      {
+        id: 5,
+        location: "Asilomar",
+        sightedAt: new Date("2019-10-14")
+      },
+      {
+        id: 6,
+        location: "Asilomar",
+        sightedAt: new Date("2019-10-14")
+      }
+    ];
+  }
 });
